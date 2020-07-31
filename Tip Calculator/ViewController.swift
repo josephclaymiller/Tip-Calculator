d1af2a4cbd97625e9a53618bc98f415d8c58ee4b
Joseph Miller <josephclaymiller@gmail.com>
2020-07-30 17:51:30 -0700

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Joseph Miller on 7/30/20.
+//  Copyright © 2020 Joseph Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
