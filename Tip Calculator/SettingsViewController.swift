9825662b1c98ee9bd0cc0b4842d93c1d665bf563
Joseph Miller <josephclaymiller@gmail.com>
2020-08-02 17:50:30 -0700

    Save settings

--- Tip Calculator/SettingsViewController.swift
+++ Tip Calculator/SettingsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    @IBOutlet weak var defaultTipSlider: UISlider!
     @IBOutlet weak var tipPercentLabel: UILabel!
     var tipPercentage = 15
 
@@ -17,12 +18,34 @@ class SettingsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        loadSettings()
+    }
+    
+    func saveSettings() {
+        // Access UserDefaults
+        let defaults = UserDefaults.standard
+        // Set int value for "defaultTip" key
+        defaults.set(tipPercentage, forKey: "defaultTip")
+        // Force UserDefaults to save
+        defaults.synchronize()
+    }
+    
+    func loadSettings() {
+        // Access UserDefaults
+        let defaults = UserDefaults.standard
+        // Get int value for tip
+        let tipValue = defaults.integer(forKey: "defaultTip")
+        tipPercentage = tipValue
+        defaultTipSlider.setValue(Float(tipValue)/100, animated: false)
+        setDefaultTip(defaultTipSlider)
     }
     
     @IBAction func setDefaultTip(_ sender: UISlider) {
         let tipSlider = sender
         tipPercentage = Int(tipSlider.value * 100)
         tipPercentLabel.text = "\(tipPercentage)%"
+        // Save application settings
+        saveSettings()
     }
     
     /*
